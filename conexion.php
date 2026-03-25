e0a6def509f725383e0b6e49decf28ba29d7ccfb
katherin julissa leyva calero <katherin.leyva@tecsup.edu.pe>
2026-03-25 15:52:04 -0500

    version5

--- conexion.php
+++ conexion.php
@@ -1,18 +1,15 @@
 <?php
-function conexion() {
-    $host = "dpg-cr6bdj1u0jms73bn1teg-a.oregon-postgres.render.com";
-    $port = "5432";
-    $dbname = "render_3rc5";
-    $user = "render_3rc5_user";
-    $password = "QU1xfoO7mi3l2mNRCGBvQfBidqreXPiB";
-    $sslmode = "require";   // obligatorio en Render
 
-    $conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password sslmode=$sslmode";
-    $db = @pg_connect($conn_string);
+    function conexion(){
 
-    if (!$db) {
-        error_log("ERROR pg_connect: " . pg_last_error());
-        return false;
-    }
+    $host = "host=dpg-d723d6gule4c7395ksrg-a.oregon-postgres.render.com";
+    $port = "port=5432";
+    $dbname = "dbname=render_3rc5";
+    $user = "user=render_3rc5_user";
+    $password = "password=QU1xfoO7mi3l2mNRCGBvQfBidqreXPiB";
+
+    $db = pg_connect("$host $port $dbname $user $password");
+
     return $db;
 }
+?>
